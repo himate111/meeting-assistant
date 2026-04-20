0d25bc2c68f419c35c46bceb31cf007e42e8ee2b
siva <sivashankar.mk16@gmail.com>
2026-04-20 23:01:56 +0530

    add Aiven SSL config and port support

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
   origin: [
     "http://localhost:3000",
-    "https://meeting-assistant.vercel.app"
+    "https://meeting-assistant-orpin.vercel.app"
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
